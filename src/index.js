6d0c7abf4a1b4a87968be4c8b902abf0a054ea90
Sergey Durov <sergssd@yandex.com>
2021-03-09 18:29:43 +0500

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  if(n > 0) return +n.toString().split('').reverse().join('');
+  if(n < 0) {
+      let num = n * -1;
+      num = num.toString().split('').reverse().join('');
+      return +num;  
+  }
 }
